a07a30e04ffa240a750035d37b88d0b6fb66bd91
mcopenha <devnull@localhost>
2001-05-19 06:59:04 +0000

    [chezbob @ 2001-05-18 23:59:04 by mcopenha] updated sql files.

--- bobmenu/SQL/bulk_items.sql
+++ bobmenu/SQL/bulk_items.sql
@@ -3,7 +3,5 @@ create table bulk_items
   bulk_barcode varchar not null,
   bulk_name varchar not null,
   item_barcode varchar not null,
-  item_quantity int not null,
-
-  unique(item_barcode)
+  item_quantity int not null
 );
---
+++ bobmenu/SQL/profiles.sql
@@ -0,0 +1,8 @@
+
+create table profiles
+(
+  userid 	int             not null,
+  property 	varchar      	not null,
+  setting 	int		not null		   
+);
+
