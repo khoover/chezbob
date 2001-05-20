82376da34271eabc00dc0da46990bbd6273afab6
mcopenha <devnull@localhost>
2001-05-20 01:02:51 +0000

    [chezbob @ 2001-05-19 18:02:51 by mcopenha] a simple table for holding info on books.

---
+++ bobmenu/SQL/books.sql
@@ -0,0 +1,11 @@
+create table books
+(
+  barcode varchar   not null,
+  isbn    varchar,
+  author  varchar  not null,
+  title   varchar   not null,
+
+  primary key (barcode),
+  unique (barcode),
+  unique (isbn)
+);
