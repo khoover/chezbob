b2234ec6bbb74f4435e8a2dd8d696f8614634458
klevchen <devnull@localhost>
2005-01-06 01:04:53 +0000

    [chezbob @ 2005-01-05 17:04:53 by klevchen]

--- bobmenu/www/finance.php
+++ bobmenu/www/finance.php
@@ -5,22 +5,34 @@
 	<TITLE>ChezBob Finance</TITLE>
 </HEAD>
 
-<BODY BGCOLOR=WHITE><TABLE BORDER=1 ALIGN=LEFT CELLSPACING=40>
+<BODY BGCOLOR=WHITE><TABLE BORDER=0 ALIGN=CENTER CELLSPACING=40>
 	<TR><TD WIDTH=400 ALIGN=LEFT>
-	<?php db_connect() ?>
+	<?php
+		$conn = pg_connect("user=bob");
+		if ($conn == 0) {
+			echo "<FONT SIZE=+1><STRONG>Failed to connect to database.</STRONG></FONT>";
+		} else {
+			// Get sum of positive balances.
+			$query = "SELECT SUM(balance) FROM balances WHERE balance > 0";
+			$result = pg_exec($conn, $query);
+			$row = pg_fetch_object($result, 0);
+			$positiveBalance = $row->sum;
+			pg_freeResult($result);
+			
+			// Get sum of negative balances.
+			$query = "SELECT SUM(-balance) FROM balances WHERE balance < 0";
+			$result = pg_exec($conn, $query);
+			$row = pg_fetch_object($result, 0);
+			$negativeBalance = $row->sum;
+			pg_freeResult($result);
+			
+			// Compute total balance and finish.
+			$totalBalance = $positiveBalance - $negativeBalance;
+			pg_close($conn);
+	?>
+	<H1>ChezBob Finance Summary</H1>
+	<P>Total of balances is <?php echo $positiveBalance; ?> - <?php echo $negativeBalance; ?> = <?php echo $totalBalance; ?>.
 	
-	<?php db_query('SELECT SUM(balance) FROM balances');
-		  $obj=db_nextObject(); ?>
-	<P>Total of balances is $<?php  echo $obj->sum; ?>.
-	
-	<?php db_query('SELECT SUM(balance) FROM balances WHERE balance > 0');
-		  $obj=db_nextObject(); ?>
-	<P>Total of positive balances is $<?php  echo $obj->sum; ?>.
-	
-	<?php db_query('SELECT SUM(balance) FROM balances WHERE balance < 0');
-		  $obj=db_nextObject(); ?>
-	<P>Total of negative balances is $<?php  echo $obj->sum; ?>.
-	
-	<?php db_close() ?>
+	<?php } // $conn != 0 ?>
 	</TD></TR>
 </TABLE></BODY>
