3cadf22c3c72ff98328d03469ea68a8da41f5c8b
klevchen <devnull@localhost>
2005-01-06 00:31:48 +0000

    [chezbob @ 2005-01-05 16:31:48 by klevchen] Added finance.php to generate finance reports.

---
+++ bobmenu/www/finance.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 3.2//EN">
+<?php include('common.php') ?>
+
+<HEAD>
+	<TITLE>ChezBob Finance</TITLE>
+</HEAD>
+
+<BODY BGCOLOR=WHITE><TABLE BORDER=0 ALIGN=CENTER CELLSPACING=40><TR><TD>
+	Hello World!<BR>
+	1+1=<?php echo 1+1 ?>
+</TD></TR></TABLE></BODY>
