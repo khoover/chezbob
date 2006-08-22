d0f8ab249e732378d7b33b5befb9b6f730da68ae
Michael Vrable <mvrable@cs.ucsd.edu>
2006-08-21 21:28:33 -0700

    Delete www/poll.  Not really sure what it was used for anyway...

--- bobmenu/www/poll/admin/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo "<HTML><HEAD><TITLE>test</title></head><body>test</body></html>";
-
-?>
--- bobmenu/www/poll/polldata/session.php
+++
@@ -1,5 +0,0 @@
-<?php
-$auth['session']="a920f2a7c125d257a61ffcee63153bb4";
-$auth['uid']="1";
-$auth['expire']="1019498171";
-?>
