ca31882e3d6c611aafbfeef620ad764b1791023c
Michael Vrable <mvrable@cs.ucsd.edu>
2006-09-02 19:13:05 -0700

    Add a script that will compact records added to the aggregate_purchases table.  It should be run by a cron job at least once a day to ensure that all records are compacted.

---
+++ admin/aggregate_maintenance.sql
@@ -0,0 +1,24 @@
+ -- Merge records in the aggregate_purchases table.  New purchases simply cause
+ -- new rows to be inserted in the table.  However, for storage efficiency we
+ -- will combine records with the same date and barcode.  We can also decrease
+ -- the resolution of the records further back in time by rounding the dates on
+ -- old records to the nearest month, but we don't yet do this.
+ --
+ -- This version only merges records created in the past two days, to bound the
+ -- work performed.  If run at least once a day, it will ensure that all
+ -- records merged.
+
+ -- NOTE: We rely on the fact that under PostgreSQL, now() returns the same
+ -- result for the duration of a transaction.
+begin;
+set transaction isolation level serializable;
+create temp table aggregated(date date, barcode text, quantity integer)
+    on commit drop;
+insert into aggregated
+    select date, barcode, sum(quantity) as quantity from aggregate_purchases
+        where date > now() - interval '2 day'
+        group by date, barcode;
+delete from aggregate_purchases where date > now() - interval '2 day';
+insert into aggregate_purchases
+    select * from aggregated where quantity <> 0;
+commit;
