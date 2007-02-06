cc2e844422cea0dbc8c2c595b7c038564be4690c
Michael Vrable <mvrable@cs.ucsd.edu>
2007-02-06 12:17:29 -0800

    Update database schema dump to reflect current database.

--- bobmenu/admin/schema.sql
+++ bobmenu/admin/schema.sql
@@ -61,20 +61,14 @@ CREATE TABLE products (
     name character varying NOT NULL,
     phonetic_name character varying NOT NULL,
     price numeric(12,2) NOT NULL,
-    stock integer NOT NULL
+    stock integer DEFAULT 0 NOT NULL,
+    bulkid integer
 );
 
 CREATE TABLE profiles (
     userid integer NOT NULL,
     property character varying NOT NULL,
     setting integer NOT NULL
-);
-
-CREATE TABLE bulk_items (
-    bulk_barcode character varying NOT NULL,
-    bulk_name character varying NOT NULL,
-    item_barcode character varying NOT NULL,
-    item_quantity integer NOT NULL
 );
 
 CREATE TABLE last_activity (
@@ -89,6 +83,7 @@ CREATE FUNCTION query_uid(character varying) RETURNS integer
 CREATE VIEW num_transactions AS
     SELECT transactions.userid, count(*) AS count FROM transactions GROUP BY transactions.userid;
 
+
 CREATE VIEW balances_check AS
     SELECT userid, balance, xactbalance FROM ((SELECT transactions.userid, (sum(transactions.xactvalue))::numeric(12,2) AS xactbalance FROM transactions GROUP BY transactions.userid) s NATURAL JOIN balances);
 
@@ -98,10 +93,32 @@ CREATE TABLE aggregate_purchases (
     quantity integer NOT NULL
 );
 
+CREATE TABLE bulk_items (
+    bulkid serial NOT NULL,
+    description text NOT NULL,
+    price numeric(12,2) NOT NULL,
+    taxable boolean NOT NULL,
+    quantity integer NOT NULL,
+    updated date DEFAULT now()
+);
+
+CREATE TABLE old_products (
+    barcode text NOT NULL,
+    name text NOT NULL,
+    phonetic_name text NOT NULL,
+    price numeric(12,2) NOT NULL,
+    stock integer NOT NULL,
+    bulkid integer
+);
+
+CREATE VIEW pricing AS
+    SELECT s.barcode, s.name, s.bulkid, s.price, s.cost, ((((s.price / s.cost) - 1.0) * (100)::numeric))::numeric(12,1) AS markup_pct, (s.price - s.cost) AS markup_amt FROM (SELECT barcode, name, bulkid, products.price, (((bulk_items.price * CASE WHEN taxable THEN 1.0775 ELSE 1.0 END) / (quantity)::numeric))::numeric(14,4) AS cost FROM (products JOIN bulk_items USING (bulkid))) s;
+
 CREATE INDEX profiles_index ON profiles USING btree (userid);
 CREATE INDEX transactions_user_index ON transactions USING btree (userid);
 CREATE INDEX transactions_time_index ON transactions USING btree (xacttime);
 CREATE UNIQUE INDEX users_username_index ON users USING btree (username);
+
 ALTER TABLE ONLY users
     ADD CONSTRAINT users_pkey PRIMARY KEY (userid);
 ALTER TABLE ONLY balances
@@ -116,3 +133,7 @@ ALTER TABLE ONLY products
     ADD CONSTRAINT products_pkey PRIMARY KEY (barcode);
 ALTER TABLE ONLY last_activity
     ADD CONSTRAINT last_activity_pkey PRIMARY KEY (userid);
+ALTER TABLE ONLY bulk_items
+    ADD CONSTRAINT bulk_items_pkey PRIMARY KEY (bulkid);
+ALTER TABLE ONLY products
+    ADD CONSTRAINT "$1" FOREIGN KEY (bulkid) REFERENCES bulk_items(bulkid);
