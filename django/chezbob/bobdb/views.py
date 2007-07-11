59dc40c5fb844c7345fbbdf0fc0a42709740f0d8
Michael Vrable <mvrable@cs.ucsd.edu>
2007-07-10 22:34:12 -0700

    Make the HTML template a bit more sophisticated (borrowing from admin).

--- django/chezbob/bobdb/views.py
+++ django/chezbob/bobdb/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render_to_response
 from chezbob.bobdb.models import Product
 
 def products(request):
-    return render_to_response('base.html', {'products': Product.objects.all()})
+    return render_to_response('base.html',
+                              {'user': request.user,
+                               'products': Product.objects.all()})
