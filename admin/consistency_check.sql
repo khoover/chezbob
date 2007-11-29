c0c96518f598be5cbe058de594a1d9b200b6714b
Michael Vrable <mvrable@cs.ucsd.edu>
2007-11-29 14:56:56 -0800

    Add database consistency check for cashcounts.

--- admin/consistency_check.sql
+++ admin/consistency_check.sql
@@ -21,3 +21,11 @@ select userid, balance from balances natural join pwd
 select 'Double-entry accounting transactions balance' as check;
 select transaction_id, sum(amount) from finance_splits
     group by transaction_id having sum(amount) <> 0.00;
+
+select 'Cashout totals are correct' as check;
+select id, cashout_id, total, expected from
+    (select 100*bill100 + 50*bill50 + 20*bill20 + 10*bill10 + 5*bill5 + bill1
+            + coin100 + 0.50*coin50 + 0.25*coin25 + 0.10*coin10 + 0.05*coin5
+            + 0.01*coin1 + other as expected, *
+        from django.cashout_cashcount) s
+    where expected <> total;
