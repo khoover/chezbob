377dd14dbb1f0e9525af42106a73611396476e0b
Michael Vrable <mvrable@cs.ucsd.edu>
2009-03-04 14:09:34 -0800

    Create script for mass price updating based on bulk item prices.

---
+++ django/chezbob/util/pricing.py
@@ -0,0 +1,39 @@
+"""Code for repricing items sold at Chez Bob."""
+
+import datetime, time, re, sys
+
+from chezbob.bobdb.models import BulkItem, Product, ProductSource, TAX_RATE
+from django.db import connection
+import django.db.transaction
+cursor = connection.cursor()
+
+markups = {
+    1: 0.19,                    # Shelves
+    2: 0.16,                    # Refrigerator
+    3: 0.12,                    # Freezer
+    4: 0.05,                    # Soda machine
+    5: 0.24,                    # Terminal
+    0: 0.15,                    # Unknown
+}
+
+def reprice():
+    def format_price_list(l):
+        if len(l) == 0:
+            return "??"
+        p = sorted(set("%.2f" % (x,) for x in l))
+        return ", ".join(p)
+
+    def price_set(s):
+        for b in s:
+            markup = markups[b.floor_location.id]
+            price = round(b.unit_price() * (1 + markup), 2)
+            old_prices = [p.price for p in b.product_set.all()]
+            print "%s\t%s\t%.2f\t%.3f" % (b.description, format_price_list(old_prices), price, b.unit_price())
+            for p in b.product_set.all():
+                p.price = price
+                p.save()
+
+    print "Product\tOld Price\tNew Price\tCost"
+    price_set(BulkItem.objects.filter(active=True).order_by('description'))
+    print
+    price_set(BulkItem.objects.filter(active=False).order_by('description'))
