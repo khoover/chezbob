672aaddafb420b7dfeea4e44c2d883834c09e883
Michael Vrable <mvrable@cs.ucsd.edu>
2009-07-14 10:23:28 -0700

    July pricing updates.

--- django/chezbob/util/pricing.py
+++ django/chezbob/util/pricing.py
@@ -8,11 +8,11 @@ import django.db.transaction
 cursor = connection.cursor()
 
 markups = {
-    1: 0.12,                    # Shelves
+    1: 0.11,                    # Shelves
     2: 0.12,                    # Refrigerator
-    3: 0.12,                    # Freezer
-    4: 0.04,                    # Soda machine
-    5: 0.20,                    # Terminal
+    3: 0.18,                    # Freezer
+    4: 0.03,                    # Soda machine
+    5: 0.22,                    # Terminal
     0: 0.12,                    # Unknown
 }
 
