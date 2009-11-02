6a5e3ec7989fe9a25f70ee32555b876c3ccea560
jcm@chezbob.ucsd.edu <jcm@chezbob.ucsd.edu>
2009-11-02 15:00:40 -0800

    Removed old+commented code from balance_before

--- django/chezbob/cashout/models.py
+++ django/chezbob/cashout/models.py
@@ -36,28 +36,12 @@ class CashOut(models.Model):
 
     @classmethod
     def balance_before(cls, cashout):
-        #cursor = connection.cursor()
-        #cursor.execute('SELECT SUM ("cashout_cashcount"."total") FROM "cashout_cashcount" INNER JOIN "cashout_cashout" ON ("cashout_cashcount"."cashout_id" = "cashout_cashout"."id") WHERE "cashout_cashout"."datetime" < %s', [cashout.datetime])
-
-        #try:
-        #    return cursor.fetchone()[0]
-        #except:
-        #    return 0
-
-        #counts = CashCount.objects.filter(cashout__datetime__lt=cashout.datetime)
         res = CashCount.objects.filter(cashout__datetime__lt=cashout.datetime).aggregate(total_sum=models.Sum('total'))
 
         try:
             return res['total_sum']
         except:
             return 0
-
-        #balance = 0
-        #print CashCount.objects.filter(cashout__datetime__lt=cashout.datetime).query.as_sql()
-        #for s in counts:
-        #    balance += s.total
-
-        #return balance
 
     def delete(self):
         CashCount.objects.filter(cashout=self).delete()
