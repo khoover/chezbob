9f654f6c0905cb14c376147ebdebfe3c331a8e3b
Nathan Bales <nbales@cs.ucsd.edu>
2010-07-24 17:54:02 -0700

    Arranged urls so that all django is under admin/ Changed the template on the database admin (now admin/db/) so that it has the same menu as the rest of the django site.

--- django/chezbob/urls.py
+++ django/chezbob/urls.py
@@ -5,34 +5,34 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Chez Bob products, pricing, and inventory
-    (r'^products/$', 'chezbob.bobdb.views.products'),
-    (r'^products/([0-9]+)/$', 'chezbob.bobdb.views.product_detail'),
-    (r'^orders/([0-9]+)/$', 'chezbob.bobdb.views.view_order'),
-    (r'^orders/([0-9]+)/update/$', 'chezbob.bobdb.views.update_order'),
-    (r'^sales/$', 'chezbob.bobdb.views.inventory'),
-    (r'^sales/([0-9]+)/$', 'chezbob.bobdb.views.inventory_detail'),
+    (r'^admin/products/$', 'chezbob.bobdb.views.products'),
+    (r'^admin/products/([0-9]+)/$', 'chezbob.bobdb.views.product_detail'),
+    (r'^admin/orders/([0-9]+)/$', 'chezbob.bobdb.views.view_order'),
+    (r'^admin/orders/([0-9]+)/update/$', 'chezbob.bobdb.views.update_order'),
+    (r'^admin/sales/$', 'chezbob.bobdb.views.inventory'),
+    (r'^admin/sales/([0-9]+)/$', 'chezbob.bobdb.views.inventory_detail'),
 
-    (r'^inventory/$', 'chezbob.bobdb.views.list_inventories'),
-    (r'^inventory/(\d{4}-\d{1,2}-\d{1,2})/$', 'chezbob.bobdb.views.take_inventory'),
-    (r'^inventory/order/$', 'chezbob.bobdb.views.estimate_order'),
-    (r'^inventory/order/print/$', 'chezbob.bobdb.views.display_order'),
+    (r'^admin/inventory/$', 'chezbob.bobdb.views.list_inventories'),
+    (r'^admin/inventory/(\d{4}-\d{1,2}-\d{1,2})/$', 'chezbob.bobdb.views.take_inventory'),
+    (r'^admin/inventory/order/$', 'chezbob.bobdb.views.estimate_order'),
+    (r'^admin/inventory/order/print/$', 'chezbob.bobdb.views.display_order'),
 
     # Accounting
-    (r'^finance[/]$', 'chezbob.finance.views.redirect'),
-    (r'^finance/accounts/$', 'chezbob.finance.views.account_list'),
-    (r'^finance/ledger/$', 'chezbob.finance.views.ledger'),
-    (r'^finance/account/(\d+)/$', 'chezbob.finance.views.ledger'),
-    (r'^finance/transaction/(\d+)/$', 'chezbob.finance.views.edit_transaction'),
-    (r'^finance/transaction/new/$', 'chezbob.finance.views.edit_transaction'),
-    (r'^finance/dump/$', 'chezbob.finance.views.gnuplot_dump'),
-    (r'^finance/xactdump/$', 'chezbob.finance.views.transaction_dump'),
+    (r'^admin/finance[/]$', 'chezbob.finance.views.redirect'),
+    (r'^admin/finance/accounts/$', 'chezbob.finance.views.account_list'),
+    (r'^admin/finance/ledger/$', 'chezbob.finance.views.ledger'),
+    (r'^admin/finance/account/(\d+)/$', 'chezbob.finance.views.ledger'),
+    (r'^admin/finance/transaction/(\d+)/$', 'chezbob.finance.views.edit_transaction'),
+    (r'^admin/finance/transaction/new/$', 'chezbob.finance.views.edit_transaction'),
+    (r'^admin/finance/dump/$', 'chezbob.finance.views.gnuplot_dump'),
+    (r'^admin/finance/xactdump/$', 'chezbob.finance.views.transaction_dump'),
 
     # Cashout
-    (r'^cashout/', include('chezbob.cashout.urls')),
+    (r'^admin/cashout/', include('chezbob.cashout.urls')),
 
     # Default admin interface for editing database
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/django/(.*)', admin.site.root),
 
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^admin/accounts/login/$', 'django.contrib.auth.views.login'),
 )
 
