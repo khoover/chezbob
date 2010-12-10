8f6d1e6c8e370d4fb432652d1bdb8b57e24a4ee4
Nathan Bales <nbales@cs.ucsd.edu>
2010-12-09 20:31:41 -0800

    created new shortcuts to streamline responding to views either as html or json. Made a unified model such that the json object returned can be the same as the dictionary bound to the form. Part 2

--- django/chezbob/ajax/views.py
+++ django/chezbob/ajax/views.py
@@ -7,45 +7,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 from chezbob.bobdb.models import BulkItem
-
-# FROM http://djangosnippets.org/snippets/1639/
-class JsonResponse(HttpResponse):
-	error = ""
-	__data = []
-	
-	def __set_data(self, data):
-		self.__data = (isinstance(data, QuerySet) or hasattr(data[0], '_meta'))\
-		 	and serializers.serialize('python', data) or data
-	
-	data = property(fset = __set_data)
-
-	def __get_container(self):
-		return json.dumps( 
-			{
-				"data": self.__data, 
-				"error":self.error,
-			}, cls = DjangoJSONEncoder)
-			
-	def __set_container(self, val):
-		pass
-	
-	_container = property(__get_container, __set_container)
-	
-	def __init__(self, *args, **kwargs):
-		kwargs["mimetype"] = "application/javascript"
-
-		if "data" in kwargs:
-			self.data = kwargs.pop("data")
-			
-		if "error" in kwargs:
-			self.error = kwargs.pop("error")
-		
-		super(JsonResponse, self).__init__(*args, **kwargs)
+from chezbob.shortcuts import render_json
 
 @login_required
 def bulk_items_json(request):
   products = BulkItem.objects.order_by('description')
-  return JsonResponse(data=products)
+  return render_json(products)
 
 #@login_required
 #def update_bulk_price(request):
