e54b8efd880ff9b053e88ccda4ddfcc0fe5b23dd
Nathan Bales <nbales@cs.ucsd.edu>
2010-12-10 23:51:24 -0800

    major updates to the user management page - go adding and deleting most transactions working

--- django/chezbob/users/forms.py
+++ django/chezbob/users/forms.py
@@ -6,32 +6,54 @@ from chezbob.bobdb.models import Product
 from chezbob.finance.models import Account
 from chezbob.users.models import User
 
+class ProfileForm(forms.Form):
+  id       = forms.CharField(widget=forms.TextInput(attrs={'disabled':'disabled'}))
+  username = forms.CharField()
+  nickname = forms.CharField()
+  email    = forms.EmailField()
+  password = forms.CharField(required=False)
+  disabled = forms.BooleanField(required=False)
+  
+class PreferencesForm(forms.Form):
+  auto_logout          = forms.BooleanField(required=False)
+  speech               = forms.BooleanField(required=False)
+  forget_which_product = forms.BooleanField(required=False)
+  skip_confirmation    = forms.BooleanField(required=False)
+
+class StatisticsForm(forms.Form):
+  last_purcahse_time = forms.CharField(widget=forms.TextInput(attrs={'disabled':'disabled'}))
+  last_deposit_time  = forms.CharField(widget=forms.TextInput(attrs={'disabled':'disabled'}))
+  balance            = forms.CharField(widget=forms.TextInput(attrs={'disabled':'disabled'}))
 
 class UserTransactionForm(forms.Form):
+  id         = forms.CharField(initial="new", widget=forms.HiddenInput)
   time       = forms.DateTimeField(initial=datetime.now)
 
 class BuyForm(UserTransactionForm):
   barcode    = forms.ModelChoiceField(
                    queryset=Product.objects.all().order_by("name"))
-  value      = forms.DecimalField()
+  price      = forms.DecimalField(required=False, 
+                                  help_text="Leave empty for current price.")
 
   def set_transaction_fields(self, transaction, messages):
     if self.is_valid():
       transaction.time  = self.cleaned_data['time']
       transaction.barcode = self.cleaned_data['barcode']
-      if self.cleaned_data['value'] is not None:
-        transaction.value = self.cleaned_data['value']
+      if self.cleaned_data['price'] is not None:
+        transaction.value = -1 * self.cleaned_data['price']
       else:
-        transaction.value = transaction.barcode.price
+        transaction.value = -1 * transaction.barcode.price
       transaction.source = "django"
       transaction.type = "BUY " + transaction.barcode.name.upper()
     else:
-      messages.errors.extend(self.errors)
-    
-  def set_form_fields(transaction):
-    return BuyForm({ 'time': transaction.xacttime,
-                     'value': transaction.value,
-                     'barcode': transaction.barcode, })
+      messages.errors(self.errors)
+  
+  @classmethod  
+  def set_form_fields(cls, transaction, messages):
+    return BuyForm({ 'id': transaction.id,
+                     'time': transaction.time,
+                     'price': -1 * transaction.value,
+                     'barcode': transaction.barcode.barcode, })
     
   
 class TransferForm(UserTransactionForm):
@@ -42,76 +64,137 @@ class TransferForm(UserTransactionForm):
                    queryset=User.objects.all().order_by("username"), 
                    required=True)
 
-  def new_transaction(self, user, messages):
-    message.add_error("new_transaction not implemented for trans-type transfer")
-
-  def update_transaction(self, user, transaction, messages):
-    message.add_error("update_transaction now implemented for trans-type transfer")
+  def set_transaction_fields(self, transaction, messages):
+    if self.is_valid():
+      transaction.time  = self.cleaned_data['time']
+      transaction.source = "django"
+      other = self.cleaned_data['other_user']
+      direction = self.cleaned_data['direction']
+      if direction == 'to':
+        transaction.value = -1 * self.cleaned_data['ammount']
+        transaction.type = "TRANSFER TO " + other.username.upper()
+      elif direction == 'from':
+        transaction.value = self.cleaned_data['ammount']
+        transaction.type = "TRANSFER FROM " + other.username.upper()
+      else:
+        messages.error("Unknown transfer direction %s." % [direction])
+    else:
+      messages.errors(self.errors)
     
-  def populate(transaction):
-    raise Exception() #not implemented
+  @classmethod  
+  def set_form_fields(cls, transaction, messages):
+    messages.warning("""The 'second half' of the transfer transaction is not
+                        yet edited by this form""")
+    return BuyForm({ 'id': transaction.id,
+                     'time': transaction.time,
+                     'value': transaction.value,
+                     'barcode': transaction.barcode, })
                   
 class AddUncountedForm(UserTransactionForm):
   ammount    = forms.DecimalField()
   repository = forms.ChoiceField(choices=[('cashbox','cashbox'),('soda','soda'),],
                                  required=True)
 
-  def new_transaction(self, user, messages):
-    message.add_error("new_transaction not implemented for trans-type add uncounted")
+  def set_transaction_fields(self, transaction, messages):
+    if self.is_valid():
+      transaction.time  = self.cleaned_data['time']
+      transaction.value = self.cleaned_data['ammount']
+      transaction.source = self.cleaned_data['repository']
+      transaction.type = "ADD"
+    else:
+      messages.errors(self.errors)
+  
+  @classmethod  
+  def set_form_fields(cls, transaction, messages):
+    return AddUncountedForm({ 'id': transaction.id,
+                              'time': transaction.time,
+                              'ammount': transaction.value,
+                              'repository': transaction.source, })
 
-  def update_transaction(self, user, transaction, messages):
-    message.add_error("update_transaction now implemented for trans-type add uncounted")
-
-  def populate(transaction):
-    raise Exception() #not implemented
-
-class ReimburseForm(forms.Form):
+class ReimburseForm(UserTransactionForm):
   ammount    = forms.DecimalField()
   account    = forms.ModelChoiceField(queryset=Account.objects.all().order_by("name"),
                                       required=True)
 
-  def new_transaction(self, user, messages):
-    message.add_error("new_transaction not implemented for trans-type payment")
+  def set_transaction_fields(self, transaction, messages):
+    if self.is_valid():
+      transaction.time  = self.cleaned_data['time']
+      transaction.value = self.cleaned_data['ammount']
+      transaction.source = "django"
+      transaction.type = "REIMBURSE FROM " + self.cleaned_data['account'].id
+    else:
+      messages.errors(self.errors)
+   
+  @classmethod   
+  def set_form_fields(cls, transaction, messages):
+    str_id = transaction.type.split()[2]
+    try:    
+      account_id = int(str_id)
+    except: 
+      return messages.error("Reimbursement account id %s invalid" % [str_id]);
+    else:   
+      account = Account.objects.get(account_id)
+      if account == None:
+        messages.warning("Reimbursement account id %s unrecognized" % [str_id])
+      return ReimburseForm({ 'id': transaction.id,
+                             'time': transaction.time,
+                             'ammount': transaction.value,
+                             'account': account, })
 
-  def update_transaction(self, user, transaction, messages):
-    message.add_error("update_transaction now implemented for trans-type payment")
-
-  def populate(transaction):
-    raise Exception() #not implemented
-
-class RefundForm(forms.Form):
+class RefundForm(UserTransactionForm):
   ammount    = forms.DecimalField()
+  repository = forms.ChoiceField(choices=[('cashbox','cashbox'),('soda','soda'),],
+                                 required=True)
 
-  def new_transaction(self, user, messages):
-    message.add_error("new_transaction not implemented for trans-type reimbursement")
-
-  def update_transaction(self, user, transaction, messages):
-    message.add_error("update_transaction now implemented for trans-type reimbursement")
-
-  def populate(transaction):
-    raise Exception() #not implemented
-
-class DonateForm(forms.Form):
-  ammount    = forms.DecimalField()
-
-  def new_transaction(self, user, messages):
-    message.add_error("new_transaction not implemented for trans-type donation")
-
-  def update_transaction(self, user, transaction, messages):
-    message.add_error("update_transaction now implemented for trans-type donation")
-
-  def populate(transaction):
-    raise Exception() #not implemented
+  def set_transaction_fields(self, transaction, messages):
+    if self.is_valid():
+      transaction.time  = self.cleaned_data['time']
+      transaction.value = -1 * self.cleaned_data['ammount']
+      transaction.source = self.cleaned_data['repository']
+      transaction.type = "REFUND"
+    else:
+      messages.errors(self.errors)
     
-class WriteOffForm(forms.Form):
+  @classmethod   
+  def set_form_fields(cls, transaction, messages):
+    return RefundForm({ 'id': transaction.id,
+                        'time': transaction.time,
+                        'ammount': -1 * transaction.value,
+                        'repository': transaction.source,  })
+
+class DonateForm(UserTransactionForm):
   ammount    = forms.DecimalField()
 
-  def new_transaction(self, user, messages):
-    message.add_error("new_transaction not implemented for trans-type donation")
+  def set_transaction_fields(self, transaction, messages):
+    if self.is_valid():
+      transaction.time  = self.cleaned_data['time']
+      transaction.value = -1 * self.cleaned_data['ammount']
+      transaction.source = "django"
+      transaction.type = "DONATION"
+    else:
+      messages.errors(self.errors)
+    
+  @classmethod   
+  def set_form_fields(cls, transaction, messages):
+    return DonateForm({ 'id': transaction.id,
+                        'time': transaction.time,
+                        'ammount': transaction.value, })
+    
+class WriteOffForm(UserTransactionForm):
+  ammount    = forms.DecimalField()
 
-  def update_transaction(self, user, transaction, messages):
-    message.add_error("update_transaction now implemented for trans-type donation")
-
-  def populate(transaction):
-    raise Exception() #not implemented
+  def set_transaction_fields(self, transaction, messages):
+    if self.is_valid():
+      transaction.time  = self.cleaned_data['time']
+      transaction.value = self.cleaned_data['ammount']
+      transaction.source = "django"
+      transaction.type = "WRITEOFF"
+    else:
+      messages.errors(self.errors)
+    
+  @classmethod   
+  def set_form_fields(cls, transaction, messages):
+    return WriteOffForm({ 'id': transaction.id,
+                          'time': transaction.time,
+                          'ammount': transaction.value, })
 
--- django/chezbob/users/views.py
+++ django/chezbob/users/views.py
@@ -7,6 +7,7 @@ from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.db import connection, transaction
+from django.db.models import F
 
 from chezbob.users.models import User, Barcode, Transaction
 from chezbob.users.forms import *
@@ -15,7 +16,7 @@ from chezbob.shortcuts import BobMessages, render_bob_messages, render_or_error,
 
 @login_required
 def user_list(request):
-  users = list(User.objects.all().order_by("name"))
+  users = list(User.objects.all().order_by("username"))
   return render_to_response('users/user_list.html',
                             {'user': request.user,
                              'title': "Users Overview",
@@ -43,6 +44,13 @@ def user_details_byname(request, username):
 
   return render_or_error('users/user_details.html', messages)                               
 
+def add_aggregate_purchase(date, barcode, quantity, value, bulkid):
+  cursor = connection.cursor()
+  cursor.execute("""INSERT INTO aggregate_purchases 
+                    (date, barcode, quantity, price, bulkid) 
+                    VALUES (%s, %s, %s, %s, %s)""",
+                 (date, barcode, quantity, value, bulkid))
+
 @transaction.commit_manually
 def new_transaction(type, bound, user, messages):
   try:
@@ -54,19 +62,29 @@ def new_transaction(type, bound, user, messages):
     bound.set_transaction_fields(tran, messages)
     if messages.has_errors():
       return
-    user.balance = F('balance') - tran.value
-    if type == 'ADD', 'REFUND', 'DONATION', 'WRITEOFF': pass
+    user.balance = F('balance') + tran.value
+    if type in ['ADD', 'REFUND', 'DONATION', 'WRITEOFF']: pass
     elif type == 'BUY':
-      cursor = connection.cursor()
-      cursor.execute("""INSERT INTO aggregate_purchases 
-                        (date, barcode, quantity, price, bulkid) 
-                        VALUES (%s, '%s', %s, %s, %s)""",
-                     (tran.time.date,
-                      tran.barcode.barcode, 1,
-                      tran.value,
-                      tran.barcode.bulkid))
+      add_aggregate_purchase(tran.time.date(), tran.barcode.barcode, 1,
+                             -1 * tran.value, tran.barcode.bulk.bulkid)
     elif type == 'TRANSFER':
-      messages.error("Add new transfer transaction not supported yet")
+      otran = Transaction()
+      otran.user = bound.cleaned_data['other_user']
+      otran.time = tran.time
+      otran.value = -1 * tran.value
+      otran.source = "django"
+      direction = bound.cleaned_data['direction']
+      if direction == 'to':
+        otran.type = "TRANSFER FROM " + user.username.upper()
+      elif direction == 'from':
+        otran.type = "TRANSFER TO " + user.username.upper()
+      else:
+        messages.error("Unknown transfer direction %s." % [direction])
+      otran.save()
+      otran.user.balance = F('balance') + otran.value
+      if not messages.has_errors():
+        otran.user.save()
+        messages.note("New transaction generated %s" % unicode(otran))
     elif type == 'REIMBURSE':
       messages.error("Add new reimburse transaction not supported yet")
     else:
@@ -75,15 +93,16 @@ def new_transaction(type, bound, user, messages):
     if not messages.has_errors():
       tran.save()
       user.save()
-    messages.note("New transaction generated %s" % unicode(transaction))
+    messages.note("New transaction generated %s" % unicode(tran))
   except Exception as e:
     transaction.rollback()
-    messages.error("Exception while creating new transaction: " + e.message)
+    messages.error("Exception while creating new transaction: " + str(e))
   else:
     transaction.commit()
     
 @transaction.commit_manually  
 def edit_transaction(bound, tran, messages):
+  return messages.error("Edit not supported yet")
   try:
     if not bound.is_valid:
       messages.errors(bound.error)
@@ -93,22 +112,56 @@ def edit_transaction(bound, tran, messages):
     messages.error("Exception while creating new transaction: " + e.message)
   else:
     transaction.commit()
+    
+    
+@transaction.commit_manually  
+def delete_transaction(tran, messages):
+  try:
+    type = tran.type.split()[0]
+    user = tran.user
+    user.balance = F('balance') - tran.value
+    if type in ['ADD', 'REFUND', 'DONATION', 'WRITEOFF']: pass
+    elif type == 'BUY':
+      add_aggregate_purchase(tran.time.date(), tran.barcode.barcode, -1,
+                             tran.value, tran.barcode.bulk.bulkid)
+    elif type == 'TRANSFER':
+      otrans = Transaction.objects.filter(time=tran.time, value=-1*tran.value)
+      if len(otrans) == 1:
+        otran = otrans[0]
+        otran.user.balance = F('balance') - otran.value
+        otran.user.save()
+        otran.delete()
+        messages.note("Transaction %s deleted" % unicode(otran))
+      else:     
+        messages.warning("""Could not uniquely identifiy transaction for
+                            inverse side of this transaction.  It must be 
+                            removed manually.""")
+    elif type == 'REIMBURSE':
+      messages.error("Delete reimburse transaction not supported yet")
+    else:
+      messages.error("Unknown transaction category %s. Cannot delete transaction" 
+                     % [type])
+    if not messages.has_errors():
+      tran.delete()
+      user.save()
+      messages.note("Transaction %s deleted" % unicode(tran))
+  except Exception as e:
+    transaction.rollback()
+    messages.error("Exception while creating new transaction: " + e.message)
+  else:
+    transaction.commit()
   
 
 @login_required
 def user_details(request, userid):
-  users = list(User.objects.filter(id=userid))
-  user, barcodes, transactions, title = None, None, None, None
-
+  user = User.objects.get(id=userid)
   messages = BobMessages()
-  if len(users) < 1:
-    messages.add_error("Error: Userid %s not found." % userid)
-  elif len(users) > 1:
-    messages.add_error("Error: Multiple users with id %s found." & userid)
-  else:
-    user = users[0]
-    barcodes = Barcode.objects.filter(user=user.id)
-    transactions = Transaction.objects.filter(user=user.id).order_by('time')
+  title = "Manage user: " + user.username
+  
+  if user is None:
+    return error(messages.add_error("Error: Userid %s not found." % userid))
+  
+  barcodes = Barcode.objects.filter(user=user.id)
   
   def make_form(type, title, form):
     form_types[type] = {'title' : title,
@@ -120,35 +173,78 @@ def user_details(request, userid):
   make_form('BUY',       'Make Buy Transaction', BuyForm)
   make_form('TRANSFER',  'Make Transfer',        TransferForm)
   make_form('ADD',       'Add Cash',             AddUncountedForm)
-  make_form('REIMBURSE', 'Issue Reimbursement',  ReimburseForm)
-  make_form('REFUND',    'Issue Refund',         RefundForm)
+  make_form('REIMBURSE', 'Issue Reimbursement [TODO]',  ReimburseForm)
+  make_form('REFUND',    'Issue Cash Refund',    RefundForm)
   make_form('DONATION',  'Donate Balance',       DonateForm)
   make_form('WRITEOFF',  'Write-off Balance',    WriteOffForm)
+  
+  def get_tran(is_str):
+    try: 
+      tran_id = int(id_str)
+    except: 
+      messages.error("Attempt to delete invalid transaction id %s" % [id_str])
+    else:
+      tran = Transaction.objects.get(id=tran_id)
+      if tran is None:
+        messages.error("Transaction id %s not found to delete." % [tran_id])
+      else:
+        return tran;
+  
+  for field_name in request.POST:
+    if field_name.startswith("delete_tran_"):
+      id_str = field_name[len("delete_tran_"):]
+      tran = get_tran(id_str)
+      if tran is not None:
+        delete_transaction(tran, messages)
+        user = User.objects.get(id=userid)
+    if field_name.startswith("edit_tran_"):
+      id_str = field_name[len("edit_tran_"):]
+      tran = get_tran(id_str)
+      if tran is not None:
+        form_type = tran.type.split()[0]
+        if form_type in form_types:
+          form = form_types[form_type]
+          form['fields'] = form['form'].set_form_fields(tran, messages)
+          if not messages.has_errors():
+            form['show'] = True
+        else:
+          messages.error("No edit form for transaction type %s" % [form_type])
+  
   
   for type in form_types:
     if type + "_save" in request.POST:
       bound = form_types[type]['form'](request.POST)
-      new_transaction(type, bound, user, messages)
-      
-  for tran in transactions:
-    if "delete_tran_" + tran.id in request.POST:
-      type = tran.type()
-      delete_transaction(tran, messages)
-    if "edit_tran_" + tran.id in request.POST:
-      type = tran.type()
-      bound = form_types[type]['form'](request.POST)
-      edit_transaction(bound, tran, messages)
-      if messages.has_errors():
-        # it is possible there is a partially changed but aborted trans
-        # refresh QuerySet to avoid this possibility
-        transactions = Transaction.objects.filter(user=user.id).order_by('time')
+      if bound.data['id'] == 'new':
+        new_transaction(type, bound, user, messages)
+      else:
+        edit_transaction(type, bound, messages)
+      user = User.objects.get(id=userid)
+
+  transactions = Transaction.objects.filter(user=user.id).order_by('-time')
 
   messages['tools'] = form_types.values()
+  
+  messages['profile_form'] = ProfileForm({ 'id'       : user.id,
+                                           'username' : user.username,
+                                           'nickname' : user.nickname,
+                                           'email'    : user.email,
+                                           'password' : user.password,
+                                           'disabled' : user.disabled, })
+                                           
+  messages['preferences_form'] = PreferencesForm(
+      { 'auto_logout'          : user.auto_logout,
+        'speech'               : user.speech,
+        'forget_which_product' : user.forget_which_product,
+        'skip_confirmation'    : user.skip_purchase_confirmation, })
+        
+  messages['stats_form'] = StatisticsForm({ 'last_purcahse_time' : user.last_purchase_time,
+                                            'last_deposit_time' : user.last_deposit_time,
+                                            'balance' : user.balance, })
 
-  return render_or_error('users/user_details.html', 
-                          messages.extend({ 'user'          : request.user,
-                                            'title'         : title,
-                                            'user_detailed' : user,
-                                            'barcodes'      : barcodes,
-                                            'transactions'  : transactions}))
+  return render_to_response('users/user_details.html', 
+                            messages.extend({ 'user'          : request.user,
+                                              'title'         : title,
+                                              'user_detailed' : user,
+                                              'barcodes'      : barcodes,
+                                              'transactions'  : transactions,}))
 
