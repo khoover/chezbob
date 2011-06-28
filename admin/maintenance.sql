68b673351c573fb654ee89f2095fcad7f41bdd65
Michael Vrable <mvrable@cs.ucsd.edu>
2011-06-28 14:12:02 -0700

    Make the maintenance script quiet
    Do not produce any output for non-error cases, so it can be run from
    crontab without daily e-mails.

--- admin/maintenance.sql
+++ admin/maintenance.sql
@@ -4,11 +4,6 @@
  --
  -- Written by Michael Vrable <mvrable@cs.ucsd.edu>
 
- -- Search for any accounts where the reported balance does not match the
- -- transaction log.  This query should return zero rows.
-select userid, balance, xactbalance from balances_check
-    where balance <> coalesce(xactbalance, 0.00);
-
  -- Delete old transactions from the transaction log, inserting appropriate
  -- initial balance transactions where needed.
 begin;
@@ -17,7 +12,6 @@ create temp table date_cutoff (time timestamp with time zone)
     on commit drop;
 insert into date_cutoff
     select date_trunc('day', now() - interval '3 months') as time;
-select * from date_cutoff;
 
 create temp table initial_balances (userid int, balance numeric(12, 2))
     on commit drop;
@@ -32,7 +26,3 @@ insert into transactions(xacttime, userid, xactvalue, xacttype)
     from initial_balances
     where balance <> 0;
 commit;
-
- -- Verify no new inconsistencies were introduced.
-select userid, balance, xactbalance from balances_check
-    where balance <> coalesce(xactbalance, 0.00);
