c4f8cb4f371ec8a7386b3a9c2274dc1f76642ab9
Keaton Mowery <kmowery@ucsd.edu>
2012-03-07 13:04:39 -0800

    removed dead code

--- soda/src/fp-server/fpserv.cpp
+++ soda/src/fp-server/fpserv.cpp
@@ -14,32 +14,13 @@
 int state;
 enum{ STATE_READ, STATE_LEARN, STATE_STOPPED };
 
-
-void handle_start_learn(std::string id) {
-
-}
-
 int main(int argc, char** argv) {
   if (sio_open(argc, argv, "FPSERV", "1.34", "") < 0)
     exit(10);
   sio_write(SIO_DATA, "SYS-ACCEPT\tFP-");
 
   state = STATE_READ;
-
-  // Figure out what he's ignoring
-  // Apparently nothing? perhaps kill this section
   std::string str;
-  /*while (sio_read(str, 300) > 0) {
-    std::cout << "here's a string: " << str << std::endl;
-    };*/
-
-  pthread_t reader_thread;
-  int reader_thread_ret;
-  char* reader_thread_name = "Fingerprint Reader Thread";
-
-  //  reader_thread_ret = pthread_create(&reader_thread, NULL, fpthread, (void*) reader_thread_name);
-
-  //pthread_join(reader_thread, NULL);
 
   int ret_val;
   while (true) {
@@ -53,7 +34,7 @@ int main(int argc, char** argv) {
    if(ret_val > 0) {
      std::string command = sio_field(str, 0);
      std::string userid = sio_field(str, 1);
-       
+
      if(command == "FP-LEARN-START"){
 
        state = STATE_LEARN;
