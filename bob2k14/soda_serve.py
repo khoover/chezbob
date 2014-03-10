e956221e5276eaeb5d41906a5793e56cb23053ba
no2chem <mwei@cs.ucsd.edu>
2014-03-10 16:31:11 -0700

    jsonify ready

--- bob2k14/soda_serve.py
+++ bob2k14/soda_serve.py
@@ -88,7 +88,7 @@ def bob_passwordlogin(username, password):
     user = User()
     user.login_password(username,password)
     sessionmanager.registerSession(SessionLocation.computer, user)
-    return json.dumps(sessionmanager.sessions[SessionLocation.computer])
+    return jsonify(to_jsonify_ready(sessionmanager.sessions[SessionLocation.computer].user))
 
 @app.route("/")
 @cross_origin()
