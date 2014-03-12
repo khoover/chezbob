157224f025f998357a7c7bc2d9fbd0eb7ea3ed9c
Michael Wei <mwei@cs.ucsd.edu>
2014-03-12 09:00:22 -0700

    fix indent

--- bob2k14/soda_session.py
+++ bob2k14/soda_session.py
@@ -120,7 +120,7 @@ class User:
                    privacy = profiles.query.filter(profiles.userid==user.userid).filter(profiles.property=="privacy").first()
                    if privacy is not None:
                           if privacy.setting == 1:
-                          self.privacy = True
+                               self.privacy = True
                    return True
              else:
                    raise Exception("Authentication Failure")
