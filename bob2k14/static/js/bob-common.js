3cbbe5625602fde83c6ebfe21d8274bf62fe5661
Michael Wei <mwei@cs.ucsd.edu>
2014-03-12 02:47:15 -0700

    use bootbox, pass username

--- bob2k14/static/js/bob-common.js
+++ bob2k14/static/js/bob-common.js
@@ -3,21 +3,21 @@ var rpc = new $.JsonRpcClient({ajaxUrl: '/api'});
 
 function notify_error(error)
 {
-	alert(error);
+	bootbox.alert("Error: " + error);
 }
 
 function handle_login()
 {
 	//capture username and password
-	var username = $("#login-username").text();
-	var password = $("#login-password").text();
+	var username = $("#login-username").val();
+	var password = $("#login-password").val();
 	
 	//silly crypt will require that we get the crypted password first for a salt.
 	var salt = ""
 	rpc.call('Bob.getcrypt', [username], function (result) {
-		alert(result);
+		bootbox.alert(result);
 		var cryptedPassword = unixCryptTD(password, result)
-		alert(cryptedPassword);
+		bootbox.alert(cryptedPassword);
 	},
 	function (error)
 	{
