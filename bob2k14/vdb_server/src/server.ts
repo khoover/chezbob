28c0c5dc107c95ab5cc6d222bfbd0c1c75bf8129
Michael Wei <mwei@cs.ucsd.edu>
2014-09-28 00:07:16 -0700

    stdout logging level =info

--- bob2k14/vdb_server/src/server.ts
+++ bob2k14/vdb_server/src/server.ts
@@ -55,7 +55,7 @@ class InitData {
                     streams: [
                     {
                         stream: process.stdout,
-                        level: "debug"
+                        level: "info"
                     },
                     {
                         stream: ringbuffer,
