e107527400258a9a7ac45d1e08012a5f467f537a
Michael Wei <mwei@cs.ucsd.edu>
2014-10-18 02:09:20 -0700

    more fp debug

--- bob2k14/soda_server/src/server.ts
+++ bob2k14/soda_server/src/server.ts
@@ -963,7 +963,7 @@ class sodad_server {
                             height: response.height
                         });
 
-                        log.error(response.fpimage);
+                        log.error(response);
                         var gsPixels = new Buffer(response.fpimage, 'base64');
                         var rgbPixels = new Buffer(response.width * response.height * 4); //RGBA
                         for (var i = 0; i < gsPixels.length; i++)
