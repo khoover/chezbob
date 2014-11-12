24868cf1cfd376df3877be882c845a6a69873a45
Dimitar Bounov <dimo@ucsd.edu>
2014-11-11 22:08:05 -0800

    fixes to setup.sh

--- devel_scripts/setup.sh
+++ devel_scripts/setup.sh
@@ -4,10 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE=$DIR/../
 
 function installPkg {
-  echo "Installing $1"
   dpkg -s $1 &> /dev/null
   if [[ $? -ne 0 ]]; then
-    echo "Package $1 not installed. Installing. This may request your superuser password"
+    echo "Installing $1. This may request your superuser password"
     sudo apt-get install $1
   else
     echo "Package $1 is already installed."
@@ -15,8 +14,14 @@ function installPkg {
 }
 
 function installGlobNpmPkg {
-  echo "Installing npm package $1 globally. This may request your superuser password"
-  sudo npm install -g $1
+  # First check if package is already globally installed
+  npm list -g | grep "$1@" > /dev/null
+  if [[ $? -ne 0 ]]; then 
+    echo "Installing npm package $1 globally. This may request your superuser password"
+    sudo npm install $1 -g 
+  else
+    echo "Npm package $1 is already globally installed"
+  fi
 }
 
 installPkg ruby-sass
@@ -24,6 +29,8 @@ installPkg redis-server
 installPkg nodejs
 installPkg nodejs-legacy
 installPkg socat
+installPkg libpq-dev
+
 
 installGlobNpmPkg forever
 installGlobNpmPkg gulp
@@ -65,4 +72,4 @@ echo "Attempting to copy test app.db from soda"
 echo "Please ensure you've setup an entry for soda in your .ssh/config"
 
 mkdir $BASE/deploy
-scp soda:/home/dimo/app.db $BASE/deploy/
+scp soda.ucsd.edu:/home/dimo/app.db $BASE/deploy/
