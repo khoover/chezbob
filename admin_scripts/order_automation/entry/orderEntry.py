09b4a90c37b713d2e82bc65bd5daa396128a1784
Dimitar Bounov <dbounov@cs.ucsd.edu>
2015-03-24 15:21:12 -0700

    Connected to DB on soda

--- admin_scripts/order_automation/entry/orderEntry.py
+++ admin_scripts/order_automation/entry/orderEntry.py
@@ -1,15 +1,16 @@
 import os
+from os.path import realpath, dirname, join
 import sys
 
-mydir = os.path.dirname(os.path.realpath(__file__))
+mydir = dirname(realpath(__file__))
 
-sys.path.append(os.path.join(mydir, '..', '..', '..' ))
-sys.path.append(os.path.join(mydir, '..', '..', '..', 'django'  ))
-sys.path.append(os.path.join(mydir, '..', '..', '..', 'django', 'chezbob' ))
-sys.path.append(os.path.join(mydir, '..', '..', '..', 'django', 'chezbob', 'orders' ))
+sys.path.append('/git/django/chezbob')
+sys.path.append(realpath(join(mydir, '..', '..', '..' )))
+sys.path.append(realpath(join(mydir, '..', '..', '..', 'django'  )))
+sys.path.append(realpath(join(mydir, '..', '..', '..', 'django', 'chezbob' )))
 
-os.environ = '/git/django/settings.py'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 print sys.path
 
-import models
+import orders.models
